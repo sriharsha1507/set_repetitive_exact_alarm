46566c06ca06f36e7669346d498bcd64348f84d9
sriharsha1507 <sriharsha1507@gmail.com>
2020-08-24 21:59:36 -0500

    Added AlarmReceiver.kt

---
+++ app/src/main/java/com/foodiedev/setrepetitiveexactalarm/receiver/AlarmReceiver.kt
@@ -0,0 +1,11 @@
+package com.foodiedev.setrepetitiveexactalarm.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+
+    }
+}
